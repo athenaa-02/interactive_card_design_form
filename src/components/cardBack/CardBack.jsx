9d80ba7688ae166d9c5775f4c0a36166d80a3063
natia <natia.karamnishvili369@law.tsu.ge>
2025-07-28 21:40:29 +0400

    feat: positioned cards

--- src/components/cardBack/CardBack.jsx
+++ src/components/cardBack/CardBack.jsx
@@ -3,7 +3,11 @@ import './CardBack.css'
 function CardBack(){
 return(
     <>
-    
+    <div className='card_back'>
+        <div className='black_line'></div>
+        <div className='code'>000</div>
+        <div className='card_patterns'></div>
+    </div>
     </>
 )
 }
