86dc3a5880534d60869d05a7373168b06158ae3d
natia <natia.karamnishvili369@law.tsu.ge>
2025-07-29 00:17:40 +0400

    working on form

--- src/components/cardInfo/CardInfo.jsx
+++ src/components/cardInfo/CardInfo.jsx
@@ -1,11 +1,36 @@
-import './CardInfo.css'
+import "./CardInfo.css";
 
-function CardInfo(){
-return (
+function CardInfo() {
+  return (
     <>
-    
+      <form>
+        <div className="card_holder">
+          <label htmlFor="name">CARDHOLDER NAME</label>
+          <input type="text" id="name" placeholder="e.g. Jane Appleseed" />
+        </div>
+        <div className="card_number">
+          <label htmlFor="number">CARD NUMBER</label>
+          <input
+            type="number"
+            id="number"
+            placeholder="e.g. 1234 5678 9123 0000"
+          />
+        </div>
+        <div className="inner_wrapper">
+          <div className="expiration_date">
+            <label htmlFor="exp_date">EXP. DATE (MM/YY)</label>
+            <input type="date" id="exp_date" />
+          </div>
+
+          <div className="cvc">
+            <label htmlFor="cvc">CVC</label>
+            <input type="number" placeholder="e.g. 123" />
+          </div>
+        </div>
+        <button type="submit">Confirm</button>
+      </form>
     </>
-)
+  );
 }
 
-export default CardInfo
+export default CardInfo;
